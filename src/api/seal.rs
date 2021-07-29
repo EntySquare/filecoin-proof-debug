a714c1a392a665f9a0fc8294b3af3b7e54c178ad
guodayang999 <33561649+guodayang999@users.noreply.github.com>
2021-07-30 04:05:37 +0800

    println prover_id sector_id

--- src/api/seal.rs
+++ src/api/seal.rs
@@ -377,8 +377,8 @@ pub fn seal_commit_phase1<T: AsRef<Path>, Tree: 'static + MerkleTreeTrait>(
 ) -> Result<SealCommitPhase1Output<Tree>> {
     let start_api = Local::now().timestamp();
     let start = Local::now().timestamp();
-    println!("C1 — prover_id: {:?}", prover_id);
-    println!("C1 — sector_id:: {:?}", sector_id);
+    // println!("C1 — prover_id: {:?}", prover_id);
+    // println!("C1 — sector_id:: {:?}", sector_id);
     info!("seal_commit_phase1:start: {:?}", sector_id);
 
     // Sanity check all input path types.
@@ -524,8 +524,8 @@ pub fn seal_commit_phase2<Tree: 'static + MerkleTreeTrait>(
     let start_api = Local::now().timestamp();
     let start = Local::now().timestamp();
     println!("C2 — prover_id -- sector_id ...");
-    println!("C2 — prover_id: {:?}", prover_id);
-    println!("C2 — sector_id:: {:?}", sector_id);
+    // println!("C2 — prover_id: {:?}", prover_id);
+    // println!("C2 — sector_id:: {:?}", sector_id);
 
     let SealCommitPhase1Output {
         vanilla_proofs,
